a11aa02ef445e5b150c47cf3026e5e34f1ece5f9
Fernando Garibay <garibaycejaf@gmail.com>
2022-05-15 16:45:41 -0500

    - CLista : "Todos las funciones fueron borradas"

--- Listas doblemente enlazadas/CLista.h
+++ Listas doblemente enlazadas/CLista.h
@@ -7,16 +7,7 @@ public:
     CLista();
     ~CLista();
     void InsertarInicio(CLibro* libro);
-    void InsertarFinal(CLibro* libro);
-    void Insertar(CLibro* libro);
-    void InsertarAntesDe(CLibro* libro, int z);
-    CNodo* buscarNodo(int z);
-    void modificarNodo(CLibro* libro, int z);
-    void visualizarNodo(int z);
-    void Borrar(int clave);
-    bool ListaVacia();
-    void Mostrar();
-    CNodo* Ultimo();
+    
 protected:
     CNodo* lista;
     CNodo* primero;
